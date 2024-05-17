3dab13255af18cdeb81203994335eb55c4cd6c74
Thien <tuthien.tran@connect.qut.edu.au>
2024-05-17 19:57:21 +1000

    fix: add utils' comments

--- src/main/java/cabbypatty/cab302_assignment/utils/Random.java
+++ src/main/java/cabbypatty/cab302_assignment/utils/Random.java
@@ -1,6 +1,16 @@
 package cabbypatty.cab302_assignment.utils;
 
+/**
+ * Utility class for generating random strings.
+ */
 public class Random {
+
+    /**
+     * Generates a random string of the specified length.
+     *
+     * @param length the length of the random string to generate
+     * @return a random string consisting of uppercase and lowercase letters and digits
+     */
     public static String randomString(int length) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder result = new StringBuilder();
