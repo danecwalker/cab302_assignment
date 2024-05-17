6785991eee4a2fc3cd73ee595787bc7dd8ef5f24
Thien <tuthien.tran@connect.qut.edu.au>
2024-05-17 19:43:19 +1000

    fix: add IUserDAO's comments, remove "public"

--- src/main/java/cabbypatty/cab302_assignment/store/IUserDAO.java
+++ src/main/java/cabbypatty/cab302_assignment/store/IUserDAO.java
@@ -4,7 +4,28 @@ import cabbypatty.cab302_assignment.model.User;
 
 import java.util.Date;
 
+/**
+ * Interface for handling user-related data access operations.
+ */
 public interface IUserDAO {
-    public User getUser(String email);
-    public User createUser(String name, String email, String password, Date dob, String gender);
+
+    /**
+     * Retrieves a user based on their email address.
+     *
+     * @param email the email address of the user to retrieve
+     * @return a User object representing the user
+     */
+
+    User getUser(String email);
+    /**
+     * Creates a new user with the provided details.
+     *
+     * @param name the name of the user
+     * @param email the email address of the user
+     * @param password the password for the user
+     * @param dob the date of birth of the user
+     * @param gender the gender of the user
+     * @return a User object representing the newly created user
+     */
+    User createUser(String name, String email, String password, Date dob, String gender);
 }
